c474797b898660d329f91c187a2885409cb8a25e
Shahed <shahed.amin456@gmail.com>
2023-06-12 17:28:40 +0600

    “testtttkkk”

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI,HTTPException
+from fastapi import Depends, FastAPI,HTTPException,security
 import schemas,services
 from sqlalchemy.orm import Session
 
@@ -13,3 +13,19 @@ async def register_user(user:schemas.UserRequest,db:Session=Depends(services.get
     # Create the user and return a token 
     db_user = await services.create_user(user=user , db=db)
     return await services.create_token(user=db_user)
+
+@app.post("/api/v1/login")
+async def login_user(
+    form_data:security.OAuth2PasswordRequestForm = Depends(),
+    db:Session=Depends(services.get_db)):
+
+    db_user=await services.login(email=form_data.username, password=form_data.password, db=db)
+    # If invalid login then throw exception
+    if not db_user:
+        raise HTTPException(status_code=401, detail="wrong login credentials")
+    return await services.create_token(db_user)
+
+
+@app.get("/api/users/current-user",response_model=schemas.UserResponse)
+async def current_user(user:schemas.UserResponse=Depends(services.current_user)):
+    return user
--- services.py
+++ services.py
@@ -3,9 +3,11 @@ import models,schemas
 from sqlalchemy.orm import Session
 from email_validator import validate_email,EmailNotValidError
 import fastapi
+import fastapi.security as _security
 import passlib.hash as _hash
 import jwt as jwt
 JWT_SECRET = "JJJJJJJJJJJJJJJJJJUEHEIUGHFFIURIUIUGRHRIURIUGHU"
+oauth2schema = _security.OAuth2PasswordBearer("/api/v1/login")
 
 def create_db():
     return base.metadata.create_all(bind=engine)
@@ -37,7 +39,6 @@ async def create_user(user:schemas.UserRequest,db:Session):
         email = email,
         phone = user.phone,
         password_hash = hashed_password
-
     )
     
     db.add(user_object)
@@ -56,3 +57,25 @@ async def create_token(user:models.UserModel):
     token =  jwt.encode(user_dict,JWT_SECRET)
     return dict(access_token=token,token_type = "bearer")
 
+
+async def login(email:str, password:str, db:Session):
+    db_user = await getUserByEmail(email=email, db=db)
+    if not db_user:
+        return False
+    if not db_user.password_verification(password=password):
+        return False
+    
+    return db_user
+
+
+async def current_user(db:Session=fastapi.Depends(get_db), token:str=fastapi.Depends(oauth2schema)):
+    try :
+        payload=jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+        # Get user by id and id is already available in the decoded user payload along with email,phone and name
+        db_user = db.query(models.UserModel).get(payload['id'])
+    except:
+        raise fastapi.HTTPException(status_code=401, detail="wrong credentials")
+    
+
+    # if all ok then return the DTO//Schema version user
+    return schemas.UserResponse.from_orm(db_user)
